79f78bd30c8a70f28ac534dbdaf97d3975af35f6
Ange-TOSSOU <angetossou.dev@gmail.com>
2024-08-06 11:06:04 +0200

    Get Started

---
+++ get_started/Main.java
@@ -0,0 +1,11 @@
+package tutorial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 1!");
+		System.out.println("Hello World 2!");
+	}
+
+}
