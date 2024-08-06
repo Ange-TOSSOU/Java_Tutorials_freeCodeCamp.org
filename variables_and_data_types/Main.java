6809d9ca9a964495937eb61530a4b40004805bb9
Ange-TOSSOU <angetossou.dev@gmail.com>
2024-08-06 11:31:24 +0200

    Variables and Data Types

---
+++ variables_and_data_types/Main.java
@@ -0,0 +1,26 @@
+package tutorial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Primitive Data Types
+		int var1 = 5;
+		double var2 = 5.0;
+		boolean var3 = true;
+		char var4 = '5';
+		
+		// Non Primitive Data Type
+		String var5 = "true";
+		
+		int var6 = var1;
+		
+		System.out.println("Var 1 : " + var1);
+		System.out.println("Var 2 : " + var2);
+		System.out.println("Var 3 : " + var3);
+		System.out.println("Var 4 : " + var4);
+		System.out.println("Var 5 : " + var5);
+		System.out.println("Var 6 : " + var6);
+	}
+
+}
